3bfc4ec51404a91163805991c656fcfee4f02d90
jitu2404 <103820854+jitu2404@users.noreply.github.com>
2023-08-30 03:01:10 +0530

    .

--- src/Pages/Dashbords/StudentDashbord.jsx
+++ src/Pages/Dashbords/StudentDashbord.jsx
@@ -2,16 +2,14 @@ import React, { useContext } from "react";
 import Form from "../../Components/Form/Form";
 import Navbar from "../../Components/Navbar/Navbar";
 import { AllContext } from "../../Context/Context";
+import StudentDetail from "../../Components/StudentDetail/StudentDetail";
 
 const StudentDashbord = () => {
-  const { formStatusCheck } = useContext(AllContext)
+  const { formStatusCheck } = useContext(AllContext);
   return (
     <>
       <Navbar />
-      {
-        formStatusCheck ? <Form /> : "Form Detail Show"
-
-      }
+      {formStatusCheck ? <Form /> : <StudentDetail />}
     </>
   );
 };
